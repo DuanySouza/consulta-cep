acdb3077eb3f76ce9f40baf7c15273ece53b7e25
Duany Souza <duanysouza01@gmail.com>
2021-04-18 17:23:45 -0300

    Estrutura do front e integração com a API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Formik, Field, Form} from 'formik';
+
+//import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core';
 
 function App() {
+  function onSubmit(values, actions) {
+    console.log('SUBMIT', values);
+  }
+
+  function onBlurCep(ev, setFieldValue) {
+    const { value } = ev.target;
+
+    const cep = value?.replace(/[^0-9]/g, '');
+
+    if (cep?.length !== 8) {
+      return alert('Cep inválido');
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (typeof data.erro != "undefined"){
+          return alert('Cep não existe!')
+        }else{
+          setFieldValue('logradouro', data.logradouro);
+          setFieldValue('bairro', data.bairro);
+          setFieldValue('complemento', data.complemento);
+          setFieldValue('cidade', data.localidade);
+          setFieldValue('uf', data.uf);
+        }
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        onSubmit={onSubmit}
+        validateOnMount
+        initialValues={{
+          cep: '',
+          logradouro: '',
+          numero: '',
+          complemento: '',
+          bairro: '',
+          cidade: '',
+          uf: '',
+        }}
+        render={({ isValid, setFieldValue }) => (
+          <Form>
+            <div className="form-control-group">
+              <label>Cep</label>
+              <Field name="cep" type="text" onBlur={(ev) => onBlurCep(ev, setFieldValue)} />
+            </div>
+            <div className="form-control-group">
+              <label>Logradouro</label>
+              <Field name="logradouro" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Número</label>
+              <Field name="numero" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Complemento</label>
+              <Field name="complemento" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>bairro</label>
+              <Field name="bairro" type="text" />
+            </div>
+            <div className="form-control-group">
+              <label>Cidade</label>
+              <Field name="cidade" type="text" />
+            </div>
+            <button type="submit" disabled={!isValid}>Enviar</button>
+          </Form>
+        )}
+      />
     </div>
   );
 }
 
-export default App;
+export default App; 
